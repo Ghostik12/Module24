189f34732b585bb113763c034a701a11c7282889
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2024-04-01 20:14:15 +0300

    Module25

---
+++ Module25/Module25/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module25
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext() 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-G7A32GR\SQLEXPRESS01;Database=EF;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ Module25/Module25/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Module25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AppContext())
+            {
+                var user1 = new User { Name = "A", Role = "Admin", Email = "AL" };
+                var user2 = new User { Name = "B", Role = "User", Email = "BL" };
+                var user3 = new User { Name = "С", Role = "User2", Email = "СL" };
+
+                db.Users.Add(user3);
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
